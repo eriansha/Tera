337760aff6bbd6cb0c9a7d5a3da2d4110d032dd8
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-04-18 11:52:03 +0700

    Initial Commit

---
+++ Tera/TeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeraApp.swift
+//  Tera
+//
+//  Created by Ivan on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
