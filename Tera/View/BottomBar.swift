fbfe89cb298ec4ed3d5fe2648fba439885c42c9c
ariosyahputra <ariosyh@gmail.com>
2023-05-02 12:07:10 +0700

    Changed "Purple" to .accentColor, Used Enum for choosing language, & Adjusted padding to textViewer

--- Tera/View/BottomBar.swift
+++ Tera/View/BottomBar.swift
@@ -10,9 +10,14 @@ import SwiftUI
 struct BottomBar: View {
     public var speechRecognizer: SpeechRecognizer
     
+    enum Language: String, CaseIterable {
+        case id = "id"
+        case en = "en"
+    }
+    
     @State private var isRecording: Bool = false
     @State private var showingOptions = false
-    @State private var selectionLanguage = "None"
+    @State private var selectionLanguage: String = "en"
     
     var body: some View {
             HStack{
@@ -24,14 +29,15 @@ struct BottomBar: View {
                     Image(systemName: "globe")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 30, height:30).foregroundColor(Color("Purple"))
+                        .frame(width: 30, height:30).foregroundColor(.accentColor)
                 }.confirmationDialog("Select Language", isPresented: $showingOptions, titleVisibility: .visible) {
-                    ForEach(["Bahasa Indonesia", "English"], id: \.self) { lang in
-                        Button(lang) {
-                            selectionLanguage = lang
+                    ForEach(Language.allCases, id: \.self) { lang in
+                        Button(lang.rawValue == "en" ? "Bahasa Indonesia" : "English") {
+                            selectionLanguage = lang.rawValue
                         }
                     }
                 }
+
 
                 //Record Button
                 Button(action: {
@@ -46,18 +52,19 @@ struct BottomBar: View {
                 }) {
                     ZStack{
                         if isRecording{
-                            RoundedRectangle(cornerRadius: 5).fill(Color("Purple"))
+                            RoundedRectangle(cornerRadius: 5).foregroundColor(.accentColor)
                                 .frame(width: 40, height: 40).padding(15)
                         }else{
                             Circle()
-                                .fill(Color("Purple"))
+                                .foregroundColor(.accentColor)
                                 .frame(width: 60, height: 60).padding(5)
             
                         }
   
                     }.overlay(
                         Circle()
-                            .stroke(Color("Purple"), lineWidth: 2)
+                            .stroke(lineWidth: 2).foregroundColor(.accentColor)
+                            
                     )
                 }.padding(.horizontal,50)
                 
@@ -70,7 +77,7 @@ struct BottomBar: View {
                     Image(systemName: "arrow.counterclockwise")
                         .resizable()
                                 .scaledToFit()
-                                .frame(width: 30, height:30).foregroundColor(Color("Purple"))
+                                .frame(width: 30, height:30).foregroundColor(.accentColor)
                 }
                 
             }
--- Tera/View/TextViewer.swift
+++ Tera/View/TextViewer.swift
@@ -21,14 +21,17 @@ struct TextViewer: View {
 
                 ScrollView{
 
+//                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
                     Text(speechRecognizer.transcript)
                         .font(.system(size: 36))
                         .fontWeight(.regular)
                         .foregroundColor(.white)
-                        .padding(35)
+                        .padding(25)
                         .frame(maxWidth: .infinity, alignment: .leading)
 
                 }
+                .padding(.bottom, 60)
+                .padding(.top, 10)
                 
             }
             .frame(width: 358, height: 470)
