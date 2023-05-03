cb9c18df559352557fc61550b9722b48515ffc4a
ariosyahputra <ariosyh@gmail.com>
2023-05-03 15:07:35 +0700

    Added padding to fullscreen modal

--- Tera/View/TextViewer.swift
+++ Tera/View/TextViewer.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TextViewer: View {
     @ObservedObject var speechRecognizer: SpeechRecognizer
-    
+    @State var showSheet = false
     var body: some View {
         
         ZStack(alignment: .bottomTrailing) {
@@ -21,14 +21,12 @@ struct TextViewer: View {
                 
                 VStack {
                     ScrollView{
-                        
                             Text(speechRecognizer.transcript)
                             .font(.system(size: 36))
                             .fontWeight(.regular)
                             .foregroundColor(.white)
                             .padding(.horizontal, 25)
                             .frame(maxWidth: .infinity, alignment: .leading)
-                        
                     }
                     .frame(height: 370)
                     
@@ -36,7 +34,7 @@ struct TextViewer: View {
                         Spacer()
                         
                         Button(action: {
-                            
+                            showSheet = true
                         }) {
                             Image(systemName: "viewfinder")
                                 .frame(width: 30, height: 29)
@@ -45,6 +43,35 @@ struct TextViewer: View {
                                 .fontWeight(.bold)
                                 .padding(.horizontal,17)
                                 
+                        }.sheet(isPresented: $showSheet) {
+                            VStack {
+                                HStack {
+                                    Spacer()
+                                    Button(action: {
+                                        showSheet = false
+                                    }, label: {
+                                        Image(systemName: "xmark")
+                                            .font(.system(size: 24))
+                                            .foregroundColor(.white)
+                                    })
+                                    .padding(.top,30)
+                                    .padding(.bottom,5)
+                                    .padding(.horizontal,25)
+                                }
+                                ScrollView {
+                                    Text(speechRecognizer.transcript)
+                                        .font(.system(size: 48))
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .foregroundColor(.white)
+                                        .padding(.horizontal,30)
+                                        .presentationCornerRadius(29)
+                                        .presentationBackground {
+                                            Image("FullscreenBG")
+                                        }
+                                }
+                                .frame(height: 670)
+                            }
+                            
                         }
                     }.padding(.bottom, -20)
                     
@@ -57,6 +84,8 @@ struct TextViewer: View {
                 color: Color.black.opacity(0.2),
                 radius: 10
             )
+        }.onAppear {
+            UIScrollView.appearance().indicatorStyle = .white
         }
     }
 }
